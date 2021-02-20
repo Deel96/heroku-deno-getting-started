2d85540e4273fb9798dc50d3102340bfd8a25b74
Dennis Eller <dennis.eller@mail.de>
2021-02-20 02:49:31 +0100

    geht

--- app.ts
+++ app.ts
@@ -14,5 +14,5 @@ const s = serve({ port: port });
 console.log("http://localhost:" + port);
 
 for await (const req of s) {
-  req.respond({ body: "HIer könnte REST stehen automatic !!!\n" });
+  req.respond({ body: "CI CD für Arme :D\n" });
 }
